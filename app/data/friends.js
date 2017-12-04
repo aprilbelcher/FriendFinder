ffbe27e2c22c57623f15bde84cd118a2a32c1c70
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-12-04 05:39:22 -0800

    added initial folders

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friendsArray = [
+    {
+        name: "Ronald McDonald",
+        photo: "https://vignette.wikia.nocookie.net/ronaldmcdonald/images/2/29/Ronald_McDonald_waving.jpg/revision/latest?cb=20150520002023",
+        chosen: [(q1).val(5),(q2).val(1),(q3).val(1),(q4).val(5),(q5).val(3),(q6).val(1),(q7).val(2),(q8).val(4),(q9).val(5),(q10).val(1)]
+
+    }
+]
