f11c22e85bce4bb4071e5f45891bd755bd4a780d
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-12-04 18:24:06 -0800

    final changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,52 @@
 var friendsArray = [
+	{
+		name: 'Scooter',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Fluffy',
+		photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
     {
-        name: "Ronald McDonald",
-        photo: "https://vignette.wikia.nocookie.net/ronaldmcdonald/images/2/29/Ronald_McDonald_waving.jpg/revision/latest?cb=20150520002023",
-        chosen: [(q1).val(5),(q2).val(1),(q3).val(1),(q4).val(5),(q5).val(3),(q6).val(1),(q7).val(2),(q8).val(4),(q9).val(5),(q10).val(1)]
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
 
-    }
-]
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,43 @@
-var friendsData = require('../data/friends.js');
+var friends	= require('../data/friends.js');
+var path = require('path');
 
-module.exports =function (app) {
+var totalDifference = 0;
 
-    app.get('/api/friends', function (req, res){
-        res.json(friendsData);
-    }),
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
 
-}
+	app.post('/api/friends', function(req, res){
+
+		var matchData = {
+			name: "",
+			image: "",
+			scoreDifference: 1000
+		};
+		var usrData 	= req.body;
+		var usrName 	= usrData.name;
+		var usrImage 	= usrData.image;
+		var usrScores 	= usrData.scores;
+
+		var totalDifference = 0;
+
+		for(var i = 0; i < friends.length; i++){
+			totalDifference = 0;
+
+			for(var j = 0; j < 10; j++) {
+				
+				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
+				if (totalDifference <= matchData.scoreDifference) {
+					matchData.name = friends[i].name;
+					matchData.photo = friends[i].photo;
+					matchData.scoreDifference = totalDifference;
+				}
+			}
+		}
+
+		friends.push(usrData);
+		res.json(matchData);
+	});
+};
+
